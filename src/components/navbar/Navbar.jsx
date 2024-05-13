9774ba6f33081b65aeb4963876f84955c5b6daf1
Kristopher-Rodriguez <k.rod.518@gmail.com>
2024-05-13 16:03:20 -0500

    created sidebar button animation

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
             <img src="/instagram.png" alt="" />
           </a>
           <a href="#">
-            <img src="/youtube.png" alt="" />
+            <img src="/linkedin.png" alt="" />
           </a>
           <a href="#">
             <img src="/github.png" alt="" />
