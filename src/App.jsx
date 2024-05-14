0b9e97c50f8e51e034d29008c1b9e9a7e4e792fa
Kristopher-Rodriguez <k.rod.518@gmail.com>
2024-05-14 18:45:58 -0500

    created parallax components and animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import "./app.scss";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/parallax/Parallax";
 
 const App = () => {
   return (
     <div>
-      <section id="Homepage">
+      <section id="Home">
         <Navbar />
         <Hero />
       </section>
-      <section>Parallax</section>
-      <section>Services</section>
-      <section id="Portfolio">Parallax</section>
+      <section id="Skills">
+        <Parallax type="skills" />
+      </section>
+      <section>Skills</section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
       <section>Portfolio1</section>
       <section>Portfolio2</section>
       <section>Portfolio3</section>
